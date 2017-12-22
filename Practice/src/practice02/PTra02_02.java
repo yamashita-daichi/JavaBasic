13e78cd6124de06413abe6012d4bb88a825364fc
yamashita daichi <yamachi0917kgu@gmail.com>
2017-12-22 17:00:43 +0900

    2=5の途中まで

--- Practice/src/practice02/PTra02_02.java
+++ Practice/src/practice02/PTra02_02.java
@@ -12,9 +12,10 @@ public class PTra02_02 {
 		System.out.println(num);
 
 		// ★ 変数numの値に30足した数を出力してください
-
-
+num+=30;
+System.out.println(num);
 		// ★ 以下のプログラムで40が出力されるようにしてください
+
 		System.out.println(num);	// ※※ この行は修正しないでください
 	}
 }
